5ceb78642334ed70b24286f69e02e79097849207
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2024-01-13 23:48:00 +0530

    Time: 4 ms (41.68%) | Memory: 7.7 MB (25.06%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -3,14 +3,13 @@ public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n= (int)nums.size(), ans=0;
         if(n<3) return 0;
-        int pDiff=nums[1]-nums[0], count=0;
+        int count=0;
         for(int i=2;i<n;++i){
-            if(nums[i]-nums[i-1]==pDiff){
+            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
                 ans+=1+count;
                 ++count;
             }
             else{
-                pDiff=nums[i]-nums[i-1];
                 count=0;
             }
         }
