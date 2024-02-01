7eed710be57e9ab8e3c4c084285e77b67b3bc508
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2024-02-01 15:00:31 +0530

    Time: 792 ms (33.73%) | Memory: 34.1 MB (39.96%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n= (int)nums.size();
+        set<vector<int>> vis;
+        unordered_map<int,int> ump;
+        vector<int> temp;
+        for(int i=0;i<=n-2;++i){
+            for(int j=i+1;j<n;++j){
+                if(ump.find(-nums[i]-nums[j])!=ump.end()){
+                    temp={nums[i],nums[j],-nums[i]-nums[j]};
+                    sort(temp.begin(),temp.end());
+                    vis.insert(temp);
+                }
+            }
+            if(ump.find(nums[i])==ump.end()) ump[nums[i]]=i;
+        }
+        return vector<vector<int>> ({vis.begin(),vis.end()});
+    }
+};
