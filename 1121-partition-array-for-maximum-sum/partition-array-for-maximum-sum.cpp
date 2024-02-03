ec828d6a0250d9221126d6263654534898d162ec
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2024-02-03 17:52:47 +0530

    Time: 12 ms (68.61%) | Memory: 10.9 MB (14.40%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -4,7 +4,7 @@ public:
         if(pos==len) return 0;
         int maxm=0;
         if(dp[pos]!=-1) return dp[pos];
-        dp[pos]=0;
+        
         for(int i=pos;i<min(pos+k,len);++i){
             maxm=max(maxm, arr[i]);
             dp[pos]=max(dp[pos],maxm*(i-pos+1)+helper(i+1, len, arr, k, dp));
