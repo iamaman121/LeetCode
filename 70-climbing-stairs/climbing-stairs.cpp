35814f7474df44dbe747bcb518d9089308acae54
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2024-03-10 06:38:41 +0530

    Time: 2 ms (47.14%) | Memory: 7 MB (30.38%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int a=1, b=2, c;
+        int a=1, b=2, c=3;
         for(int i=2;i<=n;++i){
             i<n?(c=a+b):c=0;
             a=b, b=c;
