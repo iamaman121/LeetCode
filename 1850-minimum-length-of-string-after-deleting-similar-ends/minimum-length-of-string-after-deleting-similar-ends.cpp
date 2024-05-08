7ed6900da59061185008ce8dfd0051967acdb18b
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2024-05-08 23:35:21 +0530

    Time: 27 ms (51.08%) | Memory: 13.9 MB (50.34%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        int left = 0;
-        int right = s.size() - 1;
-
-        while (left < right && s[left] == s[right]) {
-            char ch = s[left];
-            while (left <= right && s[left] == ch) {
-                left++;
-            }
-            while (right >= left && s[right] == ch) {
-                right--;
-            }
+        int beg= 0, end= (int)s.length()-1;
+        while(beg<end && s[beg]==s[end]){
+            while(beg+1<end && s[beg+1]==s[beg]) ++beg;
+            while(end-1>beg && s[end-1]==s[end]) --end;
+            ++beg; --end;
         }
-
-        return right - left + 1;
+        return end-beg+1;
     }
 };
