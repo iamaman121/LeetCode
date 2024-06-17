2b6396359eb3f318ac2244260bccb6d018e482cc
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2024-06-17 15:04:18 +0530

    Time: 3 ms (68.00%) | Memory: 6.9 MB (91.95%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for(int a=0;a<=sqrt(c);++a){
+            int b= sqrt(c-a*a);
+            if(a*a+b*b==c) return true;
+        }
+        return false;
+    }
+};
