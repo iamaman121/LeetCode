babdb11932d28ebcaf88b18484f9ee4572119756
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2024-10-13 21:00:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (91.30%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -5,7 +5,7 @@ public:
         vector<vector<int>> pascalTriangle(1,pascalRow);
         for(int i=2;i<=numRows;i++){
             pascalRow.push_back(1);
-            for(int j=(int)pascalRow.size()-2;j>0;--j){
+            for(int j=i-2;j>0;--j){
                 pascalRow[j]+= pascalRow[j-1];
             }
             pascalTriangle.push_back(pascalRow);
