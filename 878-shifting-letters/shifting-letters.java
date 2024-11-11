b0bf5674c32242eda8a6f3c61d19b14eb1528333
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2024-11-11 14:27:58 +0530

    Time: 7 ms (70.00%) | Memory: 60.7 MB (13.53%) - LeetSync

---
+++ 878-shifting-letters/shifting-letters.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String shiftingLetters(String s, int[] sft) {
+        int n = sft.length;
+        for(int i=n-2;i>=0;i--){
+            sft[i]+= sft[i+1];
+            if(sft[i]>=26) sft[i]= sft[i]%26;
+        }
+        char[] str = s.toCharArray();
+        for(int i=0;i<n;i++){
+            int val = str[i] - 'a';
+            val = (val+sft[i])%26;
+            str[i] = (char)(97+val);
+        }
+        return "".valueOf(str);
+    }
+}
