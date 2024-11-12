87ed4c65a5224c2c8600523f63d9aef7ca915479
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2024-11-12 17:56:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (88.06%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,7 +1,7 @@
 class Solution {
     public double myPow(double x, int n) {
         if(n==Integer.MIN_VALUE){
-            return x*myPow(x,n+1);
+            return (1/x)*myPow(x,n+1);
         }
         double ans= pow(x, Math.abs(n));
         if(n<0) return 1.0/ans;
