1546b5d92a7af008122d5f12bad9972d743c0083
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2024-12-01 20:42:50 +0530

    Time: 4 ms (9.64%) | Memory: 44.5 MB (15.41%) - LeetSync

---
+++ 983-validate-stack-sequences/validate-stack-sequences.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int n = pushed.length, pi = 0;
+        Stack<Integer> stk = new Stack<>();
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0;i<n;i++){
+            while(hs.contains(popped[i])==false && pi<n){
+                stk.push(pushed[pi]);
+                hs.add(pushed[pi]);
+                pi++;
+            }
+            if(stk.peek()==popped[i]){
+                stk.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
