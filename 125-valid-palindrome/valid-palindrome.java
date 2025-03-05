793d969fd44bc661e68ba1e1aeba8fab2f7ab41e
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-03-06 01:01:52 +0530

    Time: 3 ms (74.77%) | Memory: 43.2 MB (70.87%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,34 +1,22 @@
 class Solution {
+    private boolean isAlphaNumeric(char ch){
+        return ((ch>='0' && ch<='9') || (ch>='a' && ch<='z') || (ch>='A' && ch<='Z'));
+    }
     public boolean isPalindrome(String s) {
-        int sp = 0, ep = s.length()-1;
-        while(sp<ep){
-            if(!ok(s.charAt(sp))){
-                sp++;
-                continue;
+        int left=0, right=s.length()-1;
+        while(left<right){
+            while(left<right && !isAlphaNumeric(s.charAt(left))){
+                left++;
             }
-            else if(!ok(s.charAt(ep))){
-                ep--;
-                continue;
+            while(left<right && !isAlphaNumeric(s.charAt(right))){
+                right--;
             }
-            char ch1 = s.charAt(sp), ch2 = s.charAt(ep);
-            if(ch1==ch2 || isEqual(ch1, ch2)){
-                sp++; ep--;
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
             }
-            else return false;
-            
+            left++;
+            right--;
         }
         return true;
-    }
-    private static boolean ok(char ch){
-        return ((ch>=65 && ch<=90) || (ch>=97 && ch<=122) || (ch>=48 && ch<=57));
-    }
-    private static boolean isEqual(char a, char b){
-        if(a>=65 && a<=90){
-            a= (char)(a+32);
-        }
-        if(b>=65 && b<=90){
-            b= (char)(b+32);
-        }
-        return a==b;
     }
 }
