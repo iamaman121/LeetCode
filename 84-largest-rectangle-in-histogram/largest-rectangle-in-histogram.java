a6c1f9eb7582ef6f8f50c09d26a2d3d463244d32
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-03-11 13:11:01 +0530

    Time: 73 ms (45.42%) | Memory: 56.8 MB (91.61%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -7,7 +7,7 @@ class Solution {
             int pIdx=i;
             while(!stk.isEmpty() && stk.peek()[1]>ht[i]){
                 ans= Math.max(ans,stk.peek()[1]*(i-stk.peek()[0]));
-                pIdx= Math.min(pIdx, stk.peek()[0]);
+                pIdx= stk.peek()[0];
                 stk.pop();
             }
             stk.push(new int[]{pIdx, ht[i]});
