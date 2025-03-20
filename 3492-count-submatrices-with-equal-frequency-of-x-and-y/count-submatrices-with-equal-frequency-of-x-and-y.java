8e7aff5e4cb2c8113f6f541c6921e1296f434f52
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-03-20 08:51:40 +0530

    Time: 26 ms (48.33%) | Memory: 129.6 MB (41.67%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -1,36 +1,26 @@
 class Solution {
     public int numberOfSubmatrices(char[][] grid) {
         int n= grid.length, m= grid[0].length;
-        int[][] cumFreqX= new int[n][m];
-        int[][] cumFreqY= new int[n][m];
+        int[][] diffXY= new int[n][m];
+        int[] cntDot= new int[m];
+        int ans=0, dot=0;
         for(int i=0;i<n;i++){
+            dot=0;
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='X'){
-                    cumFreqX[i][j]= 1;
+                    diffXY[i][j]=1;
                 }
                 else if(grid[i][j]=='Y'){
-                    cumFreqY[i][j]= 1;
+                    diffXY[i][j]=-1;
                 }
-            }
-        } 
-        int ans=0;
-        for(int i=1;i<n;i++){
-            cumFreqX[i][0]+= cumFreqX[i-1][0];
-            cumFreqY[i][0]+= cumFreqY[i-1][0];
-        }
-        for(int j=1;j<m;j++){
-            cumFreqX[0][j]+= cumFreqX[0][j-1];
-            cumFreqY[0][j]+= cumFreqY[0][j-1];
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                cumFreqX[i][j]+= cumFreqX[i-1][j]+cumFreqX[i][j-1]-cumFreqX[i-1][j-1];
-                cumFreqY[i][j]+= cumFreqY[i-1][j]+cumFreqY[i][j-1]-cumFreqY[i-1][j-1];
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(cumFreqY[i][j]==cumFreqX[i][j] && cumFreqX[i][j]>0) ans++;
+                else{
+                    dot++;
+                    cntDot[j]+=dot;
+                }
+                if(i>=1) diffXY[i][j]+= diffXY[i-1][j];
+                if(j>=1) diffXY[i][j]+= diffXY[i][j-1];
+                if(i>=1 && j>=1) diffXY[i][j]-= diffXY[i-1][j-1];
+                if(diffXY[i][j]==0 && (i+1)*(j+1)-cntDot[j]>0) ans++;
             }
         }
         return ans;
