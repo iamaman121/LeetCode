1478ac9e7e041fcf7bfeffd22f813a90d679d6a2
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-03-21 14:08:17 +0530

    Time: 797 ms (22.68%) | Memory: 64.4 MB (57.73%) - LeetSync

--- 3444-find-the-number-of-good-pairs-ii/find-the-number-of-good-pairs-ii.java
+++ 3444-find-the-number-of-good-pairs-ii/find-the-number-of-good-pairs-ii.java
@@ -1,18 +1,28 @@
 class Solution {
     public long numberOfPairs(int[] nums1, int[] nums2, int k) {
         int[] cnt= new int[1000001];
+        int[] div= new int[1000001];
         for(int num: nums2) cnt[num]++;
+        Arrays.sort(nums2);
+        for(int i=0;i<nums2.length;i++){
+            if(i==0 || nums2[i]!=nums2[i-1]){
+                for(int j=nums2[i];j<=1000000;j+=nums2[i]){
+                    div[j]+= cnt[nums2[i]];
+                }
+            }
+        }
         long ans= 0;
         for(int num: nums1){
             if(num%k>0) continue;
             num/=k;
-            for(int i=1;i*i<=num;i++){
-                if(num%i>0) continue;
-                if(i*i<num){
-                    ans+= (long)cnt[num/i];
-                }
-                ans+= (long)cnt[i];
-            }
+            ans+= (long)div[num];
+            // for(int i=1;i*i<=num;i++){
+            //     if(num%i>0) continue;
+            //     if(i*i<num){
+            //         ans+= (long)cnt[num/i];
+            //     }
+            //     ans+= (long)cnt[i];
+            // }
         }
         return ans;
     }
