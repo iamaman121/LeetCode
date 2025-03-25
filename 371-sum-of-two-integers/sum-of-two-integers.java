db28c231fb32d32f3e7ccb5ed196c93af1519ca3
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-03-25 14:24:28 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (98.24%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -1,16 +1,13 @@
 class Solution {
     public int getSum(int a, int b) {
-        int ans=0, carry=0, mask = 0xFFFFFFFF;
+        int ans=0, carry=0;
         for(int i=0;i<32;i++){
             int aBit= (a>>i)&1;
             int bBit= (b>>i)&1;
             int cBit= aBit^bBit^carry;
             carry= (aBit+bBit+carry)>=2?1:0;
-            if(cBit!=0) ans|= (1<<i);
+            ans|= cBit<<i;
         }
-        // if (ans > 0x7FFFFFFF) {
-        // ans = ~(ans ^ mask);
-        // }
         return ans;
     }
 }
