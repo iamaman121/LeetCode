fb166a4facd2e3a46c7f68d40ef08b1c5df3d8ee
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-03-25 20:26:00 +0530

    Time: 24 ms (47.03%) | Memory: 46.1 MB (24.63%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,52 +1,26 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int t=tasks.length;
-        PriorityQueue<int[]> pq= new PriorityQueue<>((a,b)->b[1]-a[1]);
         int[] freq= new int[26];
-        int[] idle= new int[26];
         for(char ch: tasks){
             freq[ch-'A']++;
         }
-        int time=1;
-        while(t>0){
-            int maxFreq=0, idx=-1;
-            for(int i=0;i<26;i++){
-                if(freq[i]>maxFreq && time>=idle[i]){
-                    idx=i;
-                    maxFreq= freq[i];
-                }
-            }
-            if(idx!=-1){
-                // System.out.println("done "+idx+" "+ time+" "+freq[idx]);
-                freq[idx]--;
-                idle[idx]=time+n+1;
-                t--;
-                time++;
-            }
-            else{
-                int minTime=(int)1e9; maxFreq=0;
-                for(int i=0;i<26;i++){
-                    if(freq[i]==0) continue;
-                    if(idle[i]<minTime){
-                        minTime= idle[i];
-                        idx=i;
-                        maxFreq= freq[i];
-                    }
-                    else if(idle[i]==minTime && freq[i]>maxFreq){
-                        idx=i;
-                        maxFreq= freq[i];
-                    }
-                }
-                if(idx!=-1){
-                    // System.out.println("done "+idx+" "+ time+" "+freq[idx]);
-                    time= minTime;
-                    freq[idx]--;
-                    idle[idx]=time+n+1;
-                    t--;
-                    time++;
-                }
-            }
+        PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<26;i++){
+            if(freq[i]>0) pq.add(freq[i]);
         }
-        return time-1;
+        int time=0;
+        while(!pq.isEmpty()){
+            List<Integer> temp = new ArrayList<>();
+            for(int i=0;i<=n;i++){
+                if(!pq.isEmpty()){
+                    temp.add(pq.remove());
+                }
+            }
+            for(int cnt:temp){
+                if(--cnt>0) pq.add(cnt);
+            }
+            time+=pq.isEmpty()?temp.size():n+1;
+        }
+        return time;
     }
 }
