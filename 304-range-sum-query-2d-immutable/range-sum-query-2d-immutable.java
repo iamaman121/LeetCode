597662dfb755523985f70ab5666c8ed8ed773e19
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-03-27 14:28:06 +0530

    Time: 101 ms (61.09%) | Memory: 70.6 MB (46.30%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,27 +1,23 @@
 class NumMatrix {
-    int[][] mat;
-    public NumMatrix(int[][] A) {
-        int n= A.length, m= A[0].length;
-        mat= new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                mat[i][j]= A[i][j];
-            }
-        }
-        for(int i=1;i<n;i++) mat[i][0]+=mat[i-1][0];
-        for(int j=1;j<m;j++) mat[0][j]+=mat[0][j-1];
+    int[][] A;
+    public NumMatrix(int[][] matrix) {
+        int n= matrix.length, m= matrix[0].length;
+        A= new int[n][m];
+        A[0][0]= matrix[0][0];
+        for(int j=1;j<m;j++) A[0][j]=matrix[0][j]+A[0][j-1];
+        for(int i=1;i<n;i++) A[i][0]=matrix[i][0]+A[i-1][0];
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
-                mat[i][j]+=mat[i][j-1]+mat[i-1][j]-mat[i-1][j-1];
+                A[i][j]=A[i-1][j]+A[i][j-1]-A[i-1][j-1]+matrix[i][j];
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans= mat[row2][col2];
-        ans-=row1>=1?mat[row1-1][col2]:0;
-        ans-=col1>=1?mat[row2][col1-1]:0;
-        ans+=(row1>=1 && col1>=1)?mat[row1-1][col1-1]:0;
+        int ans= A[row2][col2];
+        if(row1>0) ans-=A[row1-1][col2];
+        if(col1>0) ans-=A[row2][col1-1];
+        if(row1>0 && col1>0) ans+=A[row1-1][col1-1];
         return ans;
     }
 }
