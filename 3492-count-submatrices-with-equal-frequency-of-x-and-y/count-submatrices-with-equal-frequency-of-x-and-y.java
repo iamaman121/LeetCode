289f0d8bb0537f0225aa2d5875591c665be1f228
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-03-27 18:19:36 +0530

    Time: 27 ms (46.88%) | Memory: 129.6 MB (43.75%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -1,26 +1,21 @@
 class Solution {
     public int numberOfSubmatrices(char[][] grid) {
-        int n= grid.length, m= grid[0].length;
+        int n= grid.length, m= grid[0].length, dot= 0, ans=0;
         int[][] diffXY= new int[n][m];
         int[] cntDot= new int[m];
-        int ans=0, dot=0;
         for(int i=0;i<n;i++){
             dot=0;
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='X'){
-                    diffXY[i][j]=1;
-                }
-                else if(grid[i][j]=='Y'){
-                    diffXY[i][j]=-1;
-                }
+                if(grid[i][j]=='X') diffXY[i][j]=1;
+                else if(grid[i][j]=='Y') diffXY[i][j]=-1;
                 else{
                     dot++;
-                    cntDot[j]+=dot;
+                    cntDot[j]+= dot;
                 }
-                if(i>=1) diffXY[i][j]+= diffXY[i-1][j];
-                if(j>=1) diffXY[i][j]+= diffXY[i][j-1];
-                if(i>=1 && j>=1) diffXY[i][j]-= diffXY[i-1][j-1];
-                if(diffXY[i][j]==0 && (i+1)*(j+1)-cntDot[j]>0) ans++;
+                if(i>=1) diffXY[i][j]+=diffXY[i-1][j];
+                if(j>=1) diffXY[i][j]+=diffXY[i][j-1];
+                if(i>=1 && j>=1) diffXY[i][j]-=diffXY[i-1][j-1];
+                if(diffXY[i][j]==0 && (i+1)*(j+1)!=cntDot[j]) ans++;
             }
         }
         return ans;
