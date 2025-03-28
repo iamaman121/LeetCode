f52b0a00fd16560aeb30aeaacf4a1ef12ec5b22a
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-03-28 18:25:27 +0530

    Time: 71 ms (98.49%) | Memory: 49.3 MB (51.52%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,8 +1,9 @@
 class Solution {
     public int countPrimes(int n) {
+        if(n<=2) return 0;
         boolean[] isPrime= new boolean[n+1];
         for(int i=3;i<=n;i+=2) isPrime[i]= true;
-        if(n>=2) isPrime[2]= true;
+        isPrime[2]= true;
         for(int i=3;i*i<=n;i+=2){
             if(isPrime[i]){
                 for(int j=i*i;j<=n;j+=2*i){
