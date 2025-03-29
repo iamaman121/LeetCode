b7966b0906a092cddfeba98e5054dbd4e55750bf
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-03-29 07:34:45 +0530

    Time: 160 ms (5.26%) | Memory: 57.4 MB (5.92%) - LeetSync

--- 927-sum-of-subsequence-widths/sum-of-subsequence-widths.java
+++ 927-sum-of-subsequence-widths/sum-of-subsequence-widths.java
@@ -9,14 +9,16 @@ class Solution {
         }
         return res;
     }
+    long multiply(long a, long b){
+        return (a*b)%mod;
+    }
     public int sumSubseqWidths(int[] nums) {
         long ans=0;
         int n= nums.length;
         Arrays.sort(nums);
         for(int i=0;i<n;i++){
-            ans=((pow(2,i)*(long)nums[i])%mod+ans)%mod;
-            ans=(ans-(pow(2,n-i-1)*(long)nums[i])%mod);
-            if(ans<0) ans+=mod; 
+            ans=(ans+multiply(pow(2,i),nums[i]))%mod;
+            ans=(ans-multiply(pow(2,n-i-1),nums[i])+mod)%mod;
         }
         return (int)ans;
     }
