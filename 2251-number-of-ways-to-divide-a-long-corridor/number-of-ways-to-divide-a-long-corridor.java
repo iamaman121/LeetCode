641e2b999e7c86c19122202e8dcced4e6fe5991b
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-03-29 08:31:57 +0530

    Time: 30 ms (60.31%) | Memory: 55.7 MB (22.14%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
@@ -1,16 +1,16 @@
 class Solution {
     public int numberOfWays(String corridor) {
-        List<Integer> seats= new ArrayList<>();
+        List<Integer> seatsIdx= new ArrayList<>();
         for(int i=0;i<corridor.length();i++){
-            if(corridor.charAt(i)=='S') seats.add(i);
+            if(corridor.charAt(i)=='S') seatsIdx.add(i);
         }
-        int len= seats.size();
+        int len= seatsIdx.size();
         if(len%2==1 || len==0) return 0;
-        long ans=1, curr, mod= (long)(1e9+7);
+        long res=1, curr, mod= (long)(1e9+7);
         for(int i=2;i<len;i+=2){
-            curr= seats.get(i)-seats.get(i-1);
-            ans= (ans*curr)%mod;
-        }
-        return (int)ans;
+            curr= seatsIdx.get(i)-seatsIdx.get(i-1);
+            res= (res*curr)%mod;
+        }  
+        return (int)res;
     }
 }
