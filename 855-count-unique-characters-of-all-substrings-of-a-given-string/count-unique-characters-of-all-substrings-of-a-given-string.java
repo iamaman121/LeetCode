9517ab624c9bfc051ba4aeac70b2f47b53888294
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-04 00:05:12 +0530

    Time: 39 ms (40.44%) | Memory: 47.8 MB (29.33%) - LeetSync

--- 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.java
+++ 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.java
@@ -10,12 +10,15 @@ class Solution {
         for(int alp=0;alp<26;alp++){
             temp= indexes.get(alp);
             for(int i=0;i<temp.size();i++){
+                //finding the number of substrings in which this char will contribute
+                //set left value
                 if(i==0){
                     left= temp.get(i)+1;
                 }
                 else{
                     left= temp.get(i)-temp.get(i-1);
                 }
+                //set right value
                 if(i==temp.size()-1){
                     right= s.length()-temp.get(i);
                 }
