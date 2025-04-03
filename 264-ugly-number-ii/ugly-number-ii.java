1d55571ca73878744fd89d681a02860e9b4b5fb5
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-04 00:56:33 +0530

    Time: 13 ms (40.76%) | Memory: 44.5 MB (41.05%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -1,19 +1,17 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        if(n==1) return 1;
         List<Integer> twoPtr= new ArrayList<>();
         List<Integer> threePtr= new ArrayList<>();
         List<Integer> FivePtr= new ArrayList<>();
-        twoPtr.add(2); threePtr.add(3); FivePtr.add(5); 
-        int i=0, j=0, k=0, prev=1;
-        for(int cnt=2;cnt<=n;cnt++){
+        twoPtr.add(1); threePtr.add(1); FivePtr.add(1); 
+        int i=0, j=0, k=0, prev=0;
+        while(n-->0){
             while(i<twoPtr.size() && twoPtr.get(i)<=prev) ++i;
             while(j<threePtr.size() && threePtr.get(j)<=prev) ++j;
             while(k<FivePtr.size() && FivePtr.get(k)<=prev) ++k;
             prev= Math.min(FivePtr.get(k), Math.min(twoPtr.get(i), threePtr.get(j)));
             twoPtr.add(prev*2);threePtr.add(prev*3);FivePtr.add(prev*5);
-            if(cnt==n) return prev;
         }
-        return -1;
+        return prev;
     }
 }
