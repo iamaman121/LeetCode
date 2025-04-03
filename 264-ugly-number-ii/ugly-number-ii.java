60f3cac09509efbecb03928d91d010a981ec7073
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-04 00:58:25 +0530

    Time: 14 ms (40.71%) | Memory: 44.9 MB (19.22%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -4,12 +4,12 @@ class Solution {
         List<Integer> threePtr= new ArrayList<>();
         List<Integer> FivePtr= new ArrayList<>();
         twoPtr.add(1); threePtr.add(1); FivePtr.add(1); 
-        int i=0, j=0, k=0, prev=0;
+        int i=0, j=0, k=0, prev=1;
         while(n-->0){
+            prev= Math.min(FivePtr.get(k), Math.min(twoPtr.get(i), threePtr.get(j)));
             while(i<twoPtr.size() && twoPtr.get(i)<=prev) ++i;
             while(j<threePtr.size() && threePtr.get(j)<=prev) ++j;
             while(k<FivePtr.size() && FivePtr.get(k)<=prev) ++k;
-            prev= Math.min(FivePtr.get(k), Math.min(twoPtr.get(i), threePtr.get(j)));
             twoPtr.add(prev*2);threePtr.add(prev*3);FivePtr.add(prev*5);
         }
         return prev;
