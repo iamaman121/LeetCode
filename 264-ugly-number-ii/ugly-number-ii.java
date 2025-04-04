7ae37d1b1262713b1c1f774b1d3774c5b0a8b45a
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-04 13:12:43 +0530

    Time: 2 ms (97.48%) | Memory: 41.5 MB (90.80%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -3,12 +3,13 @@ class Solution {
         int[] ugly= new int[n];
         ugly[0]=1; 
         int j=1, m2=0, m3=0, m5=0;
+
         while(j<n){
             int curr= Math.min(ugly[m2]*2,Math.min(ugly[m3]*3,ugly[m5]*5));
+            ugly[j++]= curr;
             if(curr==ugly[m2]*2) m2++;
             if(curr==ugly[m3]*3) m3++;
             if(curr==ugly[m5]*5) m5++;
-            ugly[j++]= curr;
         }
         return ugly[n-1];
     }
