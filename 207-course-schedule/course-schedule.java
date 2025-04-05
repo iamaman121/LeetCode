570a57f691a4f58b32765975d98941bc5eecccaf
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-05 20:24:20 +0530

    Time: 25 ms (7.92%) | Memory: 45.4 MB (63.94%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,29 @@
+class Solution {
+    private boolean dfs(int s, List<List<Integer>> adj, int[] vis){
+        System.out.println(s);
+        vis[s]= 1;
+        for(int d: adj.get(s)){
+            if(vis[d]==1) return false;
+            else if(vis[d]==2) continue;
+            if(!dfs(d, adj, vis)) return false;
+        }
+        vis[s]=2;
+        return true;
+    }
+    public boolean canFinish(int n, int[][] prereq) {
+        List<List<Integer>> adj= new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        int[] vis= new int[n];
+        for(int[] pr: prereq){
+            adj.get(pr[0]).add(pr[1]);
+        }
+        for(int i=0;i<n;i++){
+            if(vis[i]==0 && !dfs(i, adj, vis)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
