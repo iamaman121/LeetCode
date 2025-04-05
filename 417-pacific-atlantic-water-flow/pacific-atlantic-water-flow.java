bde9eedd1f4c53acc5713aeb69e034d214157f68
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-05 17:38:46 +0530

    Time: 4 ms (98.01%) | Memory: 46.2 MB (16.82%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -4,32 +4,31 @@ class Solution {
     private boolean ok(int r, int c){
         return r>=0 && r<n && c>=0 && c<m;
     }
-    private void dfs(int r, int c, int[][] ht, boolean[][] vis){
-        vis[r][c]= true;
+    private void dfs(int r, int c, int[][] ht, int[][] vis, int bit){
+        vis[r][c]|= bit;
         int nr, nc;
         for(int i=0;i<4;i++){
             nr= r+dir[i]; nc=c+dir[i+1];
-            if(ok(nr,nc) && !vis[nr][nc] && ht[nr][nc]>=ht[r][c]){
-                dfs(nr, nc, ht, vis);
+            if(ok(nr,nc) && (vis[nr][nc]&bit)==0 && ht[nr][nc]>=ht[r][c]){
+                dfs(nr, nc, ht, vis, bit);
             }
         }
     }
     public List<List<Integer>> pacificAtlantic(int[][] ht) {
         n= ht.length; m= ht[0].length;
-        boolean[][] A= new boolean[n][m];
-        boolean[][] P= new boolean[n][m];
+        int[][] vis= new int[n][m];
         for(int i=0;i<n;i++){
-            dfs(i, 0, ht, P);
-            dfs(i, m-1, ht, A);
+            dfs(i, 0, ht, vis, 1);
+            dfs(i, m-1, ht, vis, 2);
         }
         for(int j=0;j<m;j++){
-            dfs(0, j, ht, P);
-            dfs(n-1, j, ht, A);
+            dfs(0, j, ht, vis, 1);
+            dfs(n-1, j, ht, vis, 2);
         }
         List<List<Integer>> res = new ArrayList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(A[i][j] && P[i][j]) res.add(Arrays.asList(i, j));
+                if(vis[i][j]==3) res.add(Arrays.asList(i, j));
             }
         }
         return res;
