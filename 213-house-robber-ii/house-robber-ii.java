0c312a93253702e1c75e8126514da307556d3040
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-07 14:53:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (38.14%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -2,19 +2,19 @@ class Solution {
     public int rob(int[] nums) {
         int n= nums.length, ans=0;
         if(n==1) return nums[0];
-        int[] dp= new int[n];
-        dp[0]= nums[0]; dp[1]= Math.max(nums[1],nums[0]);
+        int one= nums[0], two= Math.max(nums[0],nums[1]), three= 0;
         for(int i=2;i<=n-2;i++){
-            dp[i]= dp[i-2]+nums[i];
-            if(dp[i]<dp[i-1]) dp[i]= dp[i-1];
+            three= one+nums[i];
+            if(three<two) three= two;
+            one= two; two= three;
+        }   
+        ans= two;
+        one=0; two= nums[1];
+        for(int i=2;i<n;i++){
+            three= one+nums[i];
+            if(three<two) three= two;
+            one= two; two= three;
         }
-        ans= dp[n-2];
-        dp= new int[n];
-        dp[1]=nums[1];
-        for(int i=2;i<=n-1;i++){
-            dp[i]= dp[i-2]+nums[i];
-            if(dp[i]<dp[i-1]) dp[i]= dp[i-1];
-        }
-        return Math.max(ans, dp[n-1]);
+        return Math.max(ans, three);
     }
 }
