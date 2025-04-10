6c94f926f5ecaf6f1a368c44b41b0d9fdd7d24f2
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-10 08:58:08 +0530

    Time: 17 ms (48.56%) | Memory: 44.2 MB (80.36%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,17 +1,14 @@
 class Solution {
-    int MAXM= (int)1e9;
     public int coinChange(int[] coins, int amount) {
         int[] dp= new int[amount+1];
-        for(int i=1;i<=amount;i++) dp[i]= MAXM;
-        Arrays.sort(coins);
-        for(int i=0;i<amount;i++){
-            if(dp[i]==MAXM) continue;
+        for(int i=1;i<=amount;i++) dp[i]= amount+1;
+        for(int i=1;i<=amount;i++){
             for(int c: coins){
-                if(i<=amount-c){
-                    dp[i+c]= Math.min(dp[i+c],1+dp[i]);
+                if(i>=c && dp[i-c]<=amount){
+                    dp[i]= Math.min(dp[i],1+dp[i-c]);
                 }
             }
         }
-        return dp[amount]==MAXM?-1:dp[amount];
+        return dp[amount]<=amount?dp[amount]:-1;
     }
 }
