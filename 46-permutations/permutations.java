34baaba39b26ac7b51d20f2ca61ccce609d3aaa9
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-11 13:44:05 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (31.63%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,24 +1,23 @@
 class Solution {
-    private void swap(List<Integer> arr, int i, int j){
-        int temp = arr.get(i);
-        arr.set(i, arr.get(j));
-        arr.set(j, temp);
+    private void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-    private void permuteUtil(List<Integer> nums, int idx, List<List<Integer>> ans){
-        if(idx==nums.size()-1){
-            ans.add(new ArrayList<>(nums));
+    private void permuteUtil(int[] nums, int idx, List<List<Integer>> ans){
+        if(idx==nums.length-1){
+            List<Integer> res= new ArrayList<>();
+            for(int num: nums) res.add(num);
+            ans.add(res);
             return;
         }
-        permuteUtil(nums, idx+1, ans);
-        for(int j=idx+1;j<nums.size();j++){
+        for(int j=idx;j<nums.length;j++){
             swap(nums, idx, j);
             permuteUtil(nums, idx+1, ans);
             swap(nums, idx, j);
         }
     }
-    public List<List<Integer>> permute(int[] arr) {
-        List<Integer> nums= new ArrayList<>();
-        for(int a: arr) nums.add(a);
+    public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         permuteUtil(nums, 0, ans);
         return ans;
