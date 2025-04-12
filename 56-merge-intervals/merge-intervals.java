21aa9e7199264489244138f826bcb19ead3d6f9c
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-12 14:24:21 +0530

    Time: 9 ms (48.71%) | Memory: 46.4 MB (83.86%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -4,12 +4,11 @@ class Solution {
         int i=0, j=1, n= intervals.length;
         while(j<n){
             if(intervals[j][0]>intervals[i][1]){
-                intervals[++i]= intervals[j];
+                intervals[++i]= intervals[j++];
             }
             else{
-                intervals[i][1]= Math.max(intervals[i][1], intervals[j][1]);
+                intervals[i][1]= Math.max(intervals[i][1], intervals[j++][1]);
             }
-            j++;
         }
         int[][] ans= new int[i+1][2];
         for(j=0;j<=i;j++) ans[j]= intervals[j];
