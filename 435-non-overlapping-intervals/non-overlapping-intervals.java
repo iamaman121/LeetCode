4597973247ff056a18586a23df74c5851aa6bde3
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-12 15:00:08 +0530

    Time: 60 ms (14.76%) | Memory: 73.5 MB (71.79%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -6,16 +6,14 @@ class Solution {
                 else return a[1]-b[1];
             }
         });
-        // for(int[] tmp: intervals) System.out.print(tmp[0]+" "+tmp[1]+" , ");
-        // return 0;
-        int i=0, j=1, n= intervals.length, cnt=0;
+        int i=0, j=1, n= intervals.length;
         while(j<n){
             if(intervals[j][0]>=intervals[i][1]){
                 intervals[++i]= intervals[j];
             }
-            else cnt++;
+            // else cnt++;
             j++;
         }
-        return cnt;
+        return n-1-i;
     }
 }
