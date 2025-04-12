1339b8c92b330123017c2aef81a3143913ae5172
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-12 14:23:43 +0530

    Time: 8 ms (86.64%) | Memory: 46.6 MB (55.01%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,11 +1,6 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, new Comparator<int[]>(){
-            public int compare(int[] a, int[] b){
-                if(a[0]==b[0]) return b[1]-a[1];
-                else return a[0]-b[0];
-            }
-        });
+        Arrays.sort(intervals, (a,b)->a[0]-b[0]);
         int i=0, j=1, n= intervals.length;
         while(j<n){
             if(intervals[j][0]>intervals[i][1]){
