05f2fc8925566a18b84dbaa4e47645f024ed4725
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-04-12 14:21:45 +0530

    Time: 9 ms (48.71%) | Memory: 46.2 MB (98.45%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,22 +2,23 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, new Comparator<int[]>(){
             public int compare(int[] a, int[] b){
-                return a[0]-b[0];
+                if(a[0]==b[0]) return b[1]-a[1];
+                else return a[0]-b[0];
             }
         });
-        ArrayList<int[]> mergedNonOverlappingIntervals= new ArrayList<>();
-        int len= 0, n=intervals.length;
-        for(int i=0;i<n;++i){
-            if(len==0 || 
-                mergedNonOverlappingIntervals.get(len-1)[1]<intervals[i][0]){
-                mergedNonOverlappingIntervals.add(intervals[i]);
-                ++len;
+        int i=0, j=1, n= intervals.length;
+        while(j<n){
+            if(intervals[j][0]>intervals[i][1]){
+                intervals[++i]= intervals[j];
             }
             else{
-                mergedNonOverlappingIntervals.get(len-1)[1]= 
-                    Math.max(mergedNonOverlappingIntervals.get(len-1)[1], intervals[i][1]);
+                intervals[i][0]= Math.min(intervals[i][0], intervals[j][0]);
+                intervals[i][1]= Math.max(intervals[i][1], intervals[j][1]);
             }
+            j++;
         }
-        return mergedNonOverlappingIntervals.toArray(new int[len][]);
+        int[][] ans= new int[i+1][2];
+        for(j=0;j<=i;j++) ans[j]= intervals[j];
+        return ans;
     }
 }
