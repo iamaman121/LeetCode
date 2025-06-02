094a9869c60cdc609ca257c91d23295dfca5adad
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-06-02 23:01:18 +0530

    Time: 1 ms (72.36%) | Memory: 45.7 MB (44.53%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,28 +1,29 @@
 class Solution {
-    public void setZeroes(int[][] A) {
-        int m = A.length, n = A[0].length;
-        boolean zerothRow = false;
-        for(int i=0;i<n;i++){
-            if(A[0][i]==0){
-                zerothRow = true;
+    public void setZeroes(int[][] matrix) {
+        int r= matrix.length, c= matrix[0].length;
+        boolean setRowZero= false;
+        for(int i=0;i<c;i++){
+            if(matrix[0][i]==0){
+                setRowZero= true;
                 break;
             }
         }
-        for(int i=1;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(A[i][j]==0){
-                    A[i][0]=0;
-                    A[0][j]=0;
+        for(int i=1;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]= 0;
+                    matrix[0][j]= 0;
                 }
             }
         }
-        for(int i=m-1;i>0;i--){
-            for(int j=n-1;j>=0;j--){
-                if(A[i][0]==0 || A[0][j]==0) A[i][j]=0;
+        for(int i=r-1;i>0;i--){
+            for(int j=c-1;j>=0;j--){
+                if(matrix[i][j]==0) continue;
+                else if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]= 0;
             }
         }
-        if(zerothRow){
-            for(int i=0;i<n;i++) A[0][i]=0;
+        if(setRowZero){
+            for(int i=0;i<c;i++) matrix[0][i]= 0;
         }
     }
 }
