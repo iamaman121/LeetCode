add83c1fc923edb8d9e55b5615d77c0c02cc5665
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-06-05 08:11:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (26.17%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,19 +1,26 @@
 class Solution {
-    private void swap(int[][] matrix, int r1, int c1, int r2, int c2){
-        int temp= matrix[r1][c1];
-        matrix[r1][c1]= matrix[r2][c2];
-        matrix[r2][c2]= temp;
+    private void swap(int[][] matrix, int row1, int col1, int row2, int col2){
+        int temp= matrix[row1][col1];
+        matrix[row1][col1]= matrix[row2][col2];
+        matrix[row2][col2]= temp;
+    }
+    private void swap(int[][] matrix, int row, int col1, int col2){
+        int temp= matrix[row][col1];
+        matrix[row][col1]= matrix[row][col2];
+        matrix[row][col2]= temp;
     }
     public void rotate(int[][] matrix) {
         int n= matrix.length;
-        for(int i=0;i<n;++i){
-            for(int j=0;j<i;++j){
-                swap(matrix, i, j, j, i);
+        int leftCol= 0, rightCol= n-1;
+        while(leftCol<rightCol){
+            for(int r=0;r<n;r++){
+                swap(matrix, r, leftCol, rightCol);
             }
+            leftCol++; rightCol--;
         }
-        for(int i=0;i<n;++i){
-            for(int j=0;j<n/2;++j){
-                swap(matrix, i, j, i, n-1-j);
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                swap(matrix, i, j, n-1-j, n-1-i);
             }
         }
     }
