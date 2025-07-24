551e125af5d900fddc1e5626b54790bff6839292
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-07-24 16:38:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (36.42%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,32 +1,31 @@
 class Solution {
-    private void swap(int[] nums, int idx1, int idx2){
-        int temp= nums[idx1];
-        nums[idx1]= nums[idx2];
-        nums[idx2]= temp;
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
-    private void reverse(int[] nums, int start, int end){
-        while(start<end){
-            swap(nums, start++, end--);
+    private void reverse(int[] arr, int sp, int ep){
+        while(sp<ep){
+            swap(arr, sp++, ep--);
         }
     }
     public void nextPermutation(int[] nums) {
-        int n= nums.length, idx= -1;
+        int n = nums.length;
+        int idx = -1;
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 idx= i;
                 break;
             }
         }
-        if(idx==-1) reverse(nums, 0, n-1);
-        else{
-            int justLargerIdx= idx+1;
-            for(int j=n-1;j>idx;j--){
-                if(nums[j]>nums[idx]){
-                    justLargerIdx= j; break;
+        if(idx!=-1){
+            for(int i=n-1;i>idx;i--){
+                if(nums[i]>nums[idx]){
+                    swap(nums, i, idx);
+                    break;
                 }
             }
-            swap(nums, idx, justLargerIdx);
-            reverse(nums, idx+1, n-1);
         }
+        reverse(nums, idx+1, n-1);
     }
 }
