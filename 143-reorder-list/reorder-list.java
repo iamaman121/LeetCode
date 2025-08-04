6642d6e6e2289c66cb92602b7747ea4d50e7f6bb
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-08-04 23:09:26 +0530

    Time: 2 ms (85.65%) | Memory: 49.2 MB (8.88%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -9,16 +9,8 @@
  * }
  */
 class Solution {
-    private ListNode getMiddleNode(ListNode head){
-        ListNode slow= head, fast= head;
-        while(fast.next!=null && fast.next.next!=null){
-            slow= slow.next;
-            fast= fast.next.next;
-        }
-        return slow;
-    }
     private ListNode reverseLL(ListNode head){
-        ListNode curr= head, nxt= null, prev= null;
+        ListNode curr = head, nxt= null, prev= null;
         while(curr!=null){
             nxt= curr.next;
             curr.next= prev;
@@ -27,20 +19,35 @@ class Solution {
         }
         return prev;
     }
-    private ListNode merge(ListNode A, ListNode B){
-        if(B==null) return A;
-        ListNode nxt= merge(A.next, B.next);
-        B.next= nxt;
-        A.next= B;
-        return A;
+    private void printLL(ListNode list){
+        while(list!=null){
+            System.out.print(list.val+" ");
+            list= list.next;
+        }
+        System.out.println();
+    }
+    private ListNode merge(ListNode a, ListNode b){
+        if(a==null && b==null) return null;
+        else if(b==null) return a;
+        ListNode head= a;
+        ListNode curr= a; a= a.next;
+        curr.next= b; curr= curr.next;
+        b= b.next; curr.next= null;
+        curr.next= merge(a, b);
+        return head;
     }
     public void reorderList(ListNode head) {
-        if(head==null || head.next==null) return ;
-        ListNode middle= getMiddleNode(head);
-        ListNode right= middle.next;
-        ListNode left= head;
-        middle.next= null;
-        right= reverseLL(right);
-        head= merge(left, right);
+        if(head== null || head.next== null || head.next.next==null) return;
+        ListNode slow= head, fast= head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow= slow.next;
+            fast= fast.next.next;
+        }
+        ListNode list1= head;
+        ListNode list2= slow.next;
+        slow.next= null;
+        list2= reverseLL(list2);
+        // printLL(list1); printLL(list2);
+        head= merge(list1, list2);
     }
 }
