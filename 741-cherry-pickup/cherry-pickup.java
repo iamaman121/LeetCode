fee9ada079e7dc8da6d794255000987d02540e6a
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-09-23 16:08:22 +0530

    Time: 19 ms (41.64%) | Memory: 44.8 MB (89.44%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.java
+++ 741-cherry-pickup/cherry-pickup.java
@@ -1,29 +1,28 @@
 class Solution {
-    private int cherryPickUpUtil(int r1, int c1, int r2, int c2, int[][] grid, int[][][][] dp){
+    private int cherryPickUpUtil(int r1, int c1, int r2, int[][] grid, int[][][] dp){
+        int c2= r1+c1-r2;
         if(r1<0 || c1<0 || r2<0 || c2<0 || grid[r1][c1]==-1 || grid[r2][c2]==-1) return (int)-1e9;
         if(r1==0 && c1==0 && r2==0 && c2==0) return grid[r1][c1];
-        if(dp[r1][c1][r2][c2]==-1){
-            int w= cherryPickUpUtil(r1-1, c1, r2-1, c2, grid, dp);
-            int x= cherryPickUpUtil(r1, c1-1, r2, c2-1, grid, dp);
-            int y= cherryPickUpUtil(r1-1, c1, r2, c2-1, grid, dp);
-            int z= cherryPickUpUtil(r1, c1-1, r2-1, c2, grid, dp);
+        if(dp[r1][c1][r2]==-1){
+            int w= cherryPickUpUtil(r1-1, c1, r2-1, grid, dp);
+            int x= cherryPickUpUtil(r1, c1-1, r2, grid, dp);
+            int y= cherryPickUpUtil(r1-1, c1, r2, grid, dp);
+            int z= cherryPickUpUtil(r1, c1-1, r2-1, grid, dp);
             int ans= Math.max(Math.max(w,x), Math.max(y,z)) + grid[r1][c1];
             if(r1!=r2 || c1!=c2) ans+= grid[r2][c2];
-            dp[r1][c1][r2][c2]= ans;
+            dp[r1][c1][r2]= ans;
         }
-        return dp[r1][c1][r2][c2];
+        return dp[r1][c1][r2];
     }
     public int cherryPickup(int[][] grid) {
         int n= grid.length;
-        int[][][][] dp = new int[n][n][n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                for(int k=0;k<n;k++){
-                    Arrays.fill(dp[i][j][k], -1);
-                }
+        int[][][] dp = new int[n][n][n];
+        for(int j=0;j<n;j++){
+            for(int k=0;k<n;k++){
+                Arrays.fill(dp[j][k], -1);
             }
         }
-        int ans= cherryPickUpUtil(n-1, n-1, n-1, n-1, grid, dp);
+        int ans= cherryPickUpUtil(n-1, n-1, n-1, grid, dp);
         return ans>0?ans:0;
     }
 }
