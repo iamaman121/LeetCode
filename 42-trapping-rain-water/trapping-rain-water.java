27bd116bf2fa1f3e52d1d270fd1a75c4c066baa1
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-09-26 21:14:31 +0530

    Time: 1 ms (60.72%) | Memory: 45.9 MB (89.22%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,17 +1,22 @@
 class Solution {
-    public int trap(int[] ht) {
-        int left=0, right=ht.length-1;
-        int trappedWater=0, lftMax=ht[left], rgtMax=ht[right];
-        while(left<right){
-            if(ht[left]<ht[right]){
-                lftMax= Math.max(lftMax,ht[left]);
-                trappedWater+= lftMax-ht[left++];
-            }
-            else{
-                rgtMax= Math.max(rgtMax,ht[right]);
-                trappedWater+= rgtMax-ht[right--];
-            }
+    public int trap(int[] height) {
+        int n= height.length;
+        int[] pmx= new int[n]; pmx[0]= height[0];
+        for(int i=1;i<n;i++){
+            pmx[i]= Math.max(pmx[i-1], height[i]);
         }
-        return trappedWater;
+        int[] smx= new int[n]; smx[n-1]= height[n-1];
+        for(int i=n-2;i>=0;i--){
+            smx[i]= Math.max(smx[i+1], height[i]);
+        }
+        return calculateTrappedRainWater(n, pmx, smx, height);
+    }
+    private int calculateTrappedRainWater(int n, int[] pmx, int[] smx, int[] height){
+        int amount=0;
+        for(int i=1;i<=n-2;i++){
+            int contri= Math.min(pmx[i-1], smx[i+1])-height[i];
+            if(contri>0) amount+= contri;
+        }
+        return amount;
     }
 }
