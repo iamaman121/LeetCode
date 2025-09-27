9011347dd409fbef1001450f297cf2ec2a97bbcc
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-09-27 14:47:05 +0530

    Time: 3 ms (99.56%) | Memory: 57.1 MB (28.84%) - LeetSync

--- 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.java
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.java
@@ -1,34 +1,17 @@
 class Solution {
     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
-        int n= nums.length;
-        int[] lftMax= new int[n];
-        Stack<Integer> stk= new Stack<>();
-        for(int i=0;i<n;i++){
-            while(!stk.isEmpty() && nums[stk.peek()]<=nums[i]) stk.pop();
-            if(stk.isEmpty()){
-                lftMax[i]= -1;
+        int ans= 0, lgei= 0, prevValidCount= 0, n= nums.length;
+        for(int ep=0;ep<n;ep++){
+            if(nums[ep]>right){
+                lgei= ep+1;
+                prevValidCount= 0;
+            }
+            else if(nums[ep]>=left && nums[ep]<=right){
+                ans+= (ep-lgei+1);
+                prevValidCount= ep-lgei+1;
             }
             else{
-                lftMax[i]= stk.peek();
-            }
-            stk.push(i);
-        }
-        stk.clear();
-        int[] rgtMax= new int[n];
-        for(int i=n-1;i>=0;i--){
-            while(!stk.isEmpty() && nums[stk.peek()]<nums[i]) stk.pop();
-            if(stk.isEmpty()){
-                rgtMax[i]= n;
-            }
-            else{
-                rgtMax[i]= stk.peek();
-            }
-            stk.push(i);
-        }
-        int ans= 0;
-        for(int i=0;i<n;i++){
-            if(nums[i]>=left && nums[i]<=right){
-                ans+= (i-lftMax[i])*(rgtMax[i]-i);
+                ans+= prevValidCount;
             }
         }
         return ans;
