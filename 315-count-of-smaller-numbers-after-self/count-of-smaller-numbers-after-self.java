82d4b421d7bea75a51650383f098b1ff69781e1b
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-10-09 12:47:30 +0530

    Time: 74 ms (61.45%) | Memory: 60.6 MB (60.11%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
@@ -1,55 +1,67 @@
-class Pair{
+class Pair {
     int elem, index;
-    Pair(int elem, int index){
-        this.elem= elem;
-        this.index= index;
+    Pair(int elem, int index) {
+        this.elem = elem;
+        this.index = index;
     }
 }
+
 class Solution {
-    private void merge(Pair[] arr, int[] cnt, int s, int m, int e){
-        int i= s, j= m+1, k= 0;
-        Pair[] tmp= new Pair[e-s+1];
-        int rightCount= 0;
-        while(i<=m && j<=e){
-            if(arr[i].elem<=arr[j].elem){
-                cnt[arr[i].index]+= rightCount;
-                tmp[k++]= arr[i++];
-            }
-            else{
+    private void merge(Pair[] arr, int[] cnt, int s, int m, int e) {
+        int i = s, j = m + 1, k = 0;
+        Pair[] tmp = new Pair[e - s + 1];
+        int rightCount = 0; // how many right-side elements are smaller
+
+        while (i <= m && j <= e) {
+            if (arr[i].elem <= arr[j].elem) {
+                // all smaller right elements before arr[i] have been counted
+                cnt[arr[i].index] += rightCount;
+                tmp[k++] = arr[i++];
+            } else {
+                // this right element is smaller than arr[i]
                 rightCount++;
-                tmp[k++]= arr[j++];
+                tmp[k++] = arr[j++];
             }
         }
-        while(i<=m){
-            cnt[arr[i].index]+= e-m;
-            tmp[k++]= arr[i++];
+
+        // remaining left elements get all smaller rights counted
+        while (i <= m) {
+            cnt[arr[i].index] += rightCount;
+            tmp[k++] = arr[i++];
         }
-        while(j<=e){
-            tmp[k++]= arr[j++];
+
+        // copy remaining right elements
+        while (j <= e) {
+            tmp[k++] = arr[j++];
         }
-        for(i=s;i<=e;i++){
-            arr[i]= tmp[i-s];
+
+        // merge back
+        for (i = s; i <= e; i++) {
+            arr[i] = tmp[i - s];
         }
     }
-    private void mergeSort(Pair[] arr, int[] cnt, int s, int e){
-        if(s>=e) return;
-        int m= (s+e)/2;
+
+    private void mergeSort(Pair[] arr, int[] cnt, int s, int e) {
+        if (s >= e) return;
+        int m = (s + e) / 2;
         mergeSort(arr, cnt, s, m);
-        mergeSort(arr, cnt, m+1, e);
+        mergeSort(arr, cnt, m + 1, e);
         merge(arr, cnt, s, m, e);
     }
+
     public List<Integer> countSmaller(int[] nums) {
-        int n= nums.length;
-        int[] cnt= new int[n];
-        Pair[] arr= new Pair[n];
-        for(int i=0;i<n;i++){
-            arr[i]= new Pair(nums[i], i);
+        int n = nums.length;
+        int[] cnt = new int[n];
+        Pair[] arr = new Pair[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = new Pair(nums[i], i);
         }
-        mergeSort(arr, cnt, 0, n-1);
-        List<Integer> ans= new ArrayList<>();
-        for(int c: cnt){
-            ans.add(c);
-        }
+
+        mergeSort(arr, cnt, 0, n - 1);
+
+        List<Integer> ans = new ArrayList<>();
+        for (int c : cnt) ans.add(c);
         return ans;
     }
 }
