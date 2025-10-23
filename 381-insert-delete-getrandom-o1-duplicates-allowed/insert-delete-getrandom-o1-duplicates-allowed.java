1edb624d6af58133a0413ec44003936c8ca1d2ed
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-10-23 10:50:22 +0530

    Time: 30 ms (91.36%) | Memory: 92 MB (60.19%) - LeetSync

---
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.java
@@ -0,0 +1,53 @@
+class RandomizedCollection {
+    HashMap<Integer,HashSet<Integer>> store;
+    List<Integer> list;
+    Random r;
+
+    public RandomizedCollection() {
+        store= new HashMap<>();
+        list= new ArrayList<>();
+        r= new Random();
+    }
+    
+    public boolean insert(int val) {
+        boolean ans= false;
+        HashSet<Integer> set= store.getOrDefault(val, new HashSet<>()); 
+        if(set.size()==0) ans= true;
+        set.add(list.size()); list.add(val);
+        store.put(val, set);
+        return ans;
+    }
+    
+    public boolean remove(int val) {
+        if(store.containsKey(val)== false || store.get(val).size()==0) return false;
+        int lastIdx= list.size()-1, lastVal= list.get(lastIdx);
+        if(lastVal==val){
+            store.get(val).remove(lastIdx);
+            list.remove(lastIdx);
+        }
+        else{
+            int idx= 0;
+            for(int x: store.get(val)){
+                idx= x; break;
+            }
+            store.get(val).remove(idx);
+            store.get(lastVal).remove(lastIdx);
+            store.get(lastVal).add(idx);
+            list.set(idx, lastVal);
+            list.remove(lastIdx);
+        }
+        return true;
+    }
+    
+    public int getRandom() {
+        return list.get(r.nextInt(0, list.size()));
+    }
+}
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection obj = new RandomizedCollection();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
