28f7db7b293617f46d598b55aec6455c45ba2c35
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-10-25 18:41:39 +0530

    Time: 3 ms (87.27%) | Memory: 44.7 MB (80.44%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,64 +1,31 @@
 class Solution {
     public String minWindow(String s, String t) {
         if(s.length()<t.length()) return "";
-        int[] lalp= new int[26];
-        int[] ualp= new int[26];
-        for(int i=0;i<t.length();i++){
-            char ch = t.charAt(i);
-            if(ch>='a' && ch<='z'){
-                lalp[ch-'a']++;
+        int sp=0, ep=0, lenT= t.length();
+        int asp=-1, aep= -1, count= 0;
+        int hms[]= new int[256];
+        int hmt[]= new int[256];
+        for(char ch: t.toCharArray()){
+            hmt[ch]++;
+        }
+        for(ep=0;ep<s.length();ep++){
+            char ch= s.charAt(ep);
+            hms[ch]++;
+            if(hms[ch]<=hmt[ch]) ++count;
+            char ch2= s.charAt(sp);
+            while(hms[ch2]>hmt[ch2]){
+                hms[ch2]--; sp++;
+                if(sp>ep) break;
+                ch2= s.charAt(sp);
             }
-            else{
-                ualp[ch-'A']++;
+            if(count==lenT){
+                if((asp==-1 && aep==-1) || ep-sp<aep-asp){
+                    asp= sp; aep= ep;
+                }
+                hms[ch2]--; sp++; count--;
             }
         }
-        for(int i=0;i<t.length()-1;i++){
-            char ch = s.charAt(i);
-            if(ch>='a' && ch<='z'){
-                lalp[ch-'a']--;
-            }
-            else{
-                ualp[ch-'A']--;
-            }
-        }
-        int clft=0, alft=-1, argt=s.length()-1;
-        for(int j=t.length()-1;j<s.length();j++){
-
-            char chr = s.charAt(j);
-            if(chr>='a' && chr<='z'){
-                lalp[chr-'a']--;
-            }
-            else{
-                ualp[chr-'A']--;
-            }
-            boolean ans = true;
-            for(int i=0;i<26;i++){
-                if(lalp[i]>0 || ualp[i]>0){
-                    ans=false;
-                    break;
-                }
-            }
-            while(ans){
-                if(j-clft+1<argt-alft+1){
-                    alft=clft; argt=j;
-                }
-
-                char ch = s.charAt(clft++);
-                if(ch>='a' && ch<='z'){
-                    lalp[ch-'a']++;
-                }
-                else{
-                    ualp[ch-'A']++;
-                }
-
-                for(int i=0;i<26;i++){
-                    if(lalp[i]>0 || ualp[i]>0){
-                        ans=false;
-                        break;
-                    }
-                }
-            }
-        }
-        return alft==-1?"":s.substring(alft, argt+1);
+        if(asp==-1) return "";
+        return s.substring(asp, aep+1);
     }
 }
