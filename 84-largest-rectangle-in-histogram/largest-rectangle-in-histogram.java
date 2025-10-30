b651f087b8cf3418f312763aa392f991a95ba106
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-10-30 19:10:12 +0530

    Time: 83 ms (37.00%) | Memory: 58.3 MB (13.02%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,20 +1,38 @@
 class Solution {
-    public int largestRectangleArea(int[] ht) {
-        int n= ht.length, ans=0;
-        // [index, height]
-        Stack<int[]> stk= new Stack<>();
+    private int[] nseRight(int[] ht){
+        int n= ht.length;
+        int[] nser= new int[n];
+        Arrays.fill(nser, n);
+        Stack<Integer> stk= new Stack<>();
         for(int i=0;i<n;i++){
-            int pIdx=i;
-            while(!stk.isEmpty() && stk.peek()[1]>ht[i]){
-                ans= Math.max(ans,stk.peek()[1]*(i-stk.peek()[0]));
-                pIdx= stk.peek()[0];
-                stk.pop();
+            while(stk.size()>0 && ht[stk.peek()]>ht[i]){
+                nser[stk.pop()]= i;
             }
-            stk.push(new int[]{pIdx, ht[i]});
+            stk.push(i);
         }
-        while(!stk.isEmpty()){
-            ans= Math.max(ans,stk.peek()[1]*(n-stk.peek()[0]));
-            stk.pop();
+        return nser;
+    }
+    private int[] nseLeft(int[] ht){
+        int n= ht.length;
+        int[] nsel= new int[n];
+        Arrays.fill(nsel, -1);
+        Stack<Integer> stk= new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(stk.size()>0 && ht[stk.peek()]>ht[i]){
+                nsel[stk.pop()]= i;
+            }
+            stk.push(i);
+        }
+        return nsel;
+    }
+    public int largestRectangleArea(int[] heights) {
+        int[] nseR= nseRight(heights);
+        int[] nseL= nseLeft(heights);
+        int ans= 0;
+        for(int i=0;i<heights.length;i++){
+            int leftIdx= nseL[i]+1, rightIdx= nseR[i]-1;
+            int width= rightIdx-leftIdx+1;
+            ans= Math.max(ans, width*heights[i]);
         }
         return ans;
     }
