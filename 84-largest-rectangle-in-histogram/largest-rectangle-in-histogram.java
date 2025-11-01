9adacc543524cdcbd55721764ce2f0bd92d543b4
iamaman121 <95397381+iamaman121@users.noreply.github.com>
2025-11-01 10:54:01 +0530

    Time: 74 ms (40.47%) | Memory: 78.2 MB (5.18%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,26 +1,18 @@
-class Pair{
-    int h, v;
-    Pair(int h, int v){
-        this.h= h;
-        this.v= v;
-    }
-}
 class Solution {
     public int largestRectangleArea(int[] ht) {
-        int n= ht.length, ans=0;
-        Stack<Pair> stk = new Stack<>();
-        for(int i=0;i<n;i++){
-            int idx= i;
-            while(stk.size()>0 && stk.peek().h>ht[i]){
-                Pair p= stk.pop();
-                idx= p.v;
-                ans= Math.max(ans, (i-idx)*p.h);
+        int n= ht.length, ans= 0;
+        Stack<Integer> stk = new Stack<>();
+        for(int i=0;i<=n;i++){
+            int temp= 0;
+            if(i<n) temp= ht[i];
+            while(stk.size()>0 && temp<ht[stk.peek()]){
+                int tbs= stk.pop();
+                int nsr= i, nsl= -1;
+                if(stk.size()>0) nsl= stk.peek();
+                int width= (nsr-1)-(nsl+1)+1;
+                ans= Math.max(ans, ht[tbs]*width);
             }
-            stk.push(new Pair(ht[i], idx));
-        }
-        while(stk.size()>0){
-            Pair p= stk.pop();
-            ans= Math.max(ans, (n-p.v)*p.h);
+            stk.push(i);
         }
         return ans;
     }
